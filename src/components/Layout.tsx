a58a01981cc9e7a5f7dfbb34d7a6e9f9feed3ba1
amolkanago <kanagoamol@github.com>
2024-12-03 14:04:12 +0530

    Layout Changed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -43,14 +43,14 @@ export default function Layout() {
     <div className="min-h-screen bg-gray-50">
       {/* Top Navigation Bar */}
       <nav className="bg-white border-b border-gray-200">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex">
               <div className="flex-shrink-0 flex items-left">
                 {/* <Shield className="h-8 w-8 text-indigo-600" />
                 <span className="ml-2 text-xl font-bold text-gray-900">P2P Connect</span> */}
                 <img className='w-full h-full rounded-full' src={Logo} alt="Health-chain logo" />
-                <span className="ml-2 text-xl font-bold text-gray-900">P2P Connect</span> 
+                {/* <span className="ml-2 text-xl font-bold text-gray-900">P2P Connect</span> */}
               </div>
             </div>
             <div className="flex items-center">
